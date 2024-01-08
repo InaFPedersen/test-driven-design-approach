3651d7153a62b505ce30caf2788baea4a73ad6fd
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-01-08 08:32:08 +0100

    Added test for topbar

---
+++ test-driven-design-approach/cypress/e2e/Navigation.cy.ts
@@ -0,0 +1,29 @@
+describe("The header should have", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000");
+    });
+    it("have a logo", () => {
+        cy.get('$[data-cy="logo"]');
+    });
+    it("have a title", () => {
+        cy.get("h1").should("have.text", "Test application");
+    });
+    it("have a home tab", () => {
+        cy.get('$[data-cy="home-tab"]').should("have.text", "Home");
+    });
+    it("have a about tab", () => {
+        cy.get('$[data-cy="about-tab"]').should("have.text", "About");
+    });
+    it("have a application tab", () => {
+        cy.get('$[data-cy="application-tab"]').should(
+            "have.text",
+            "Application"
+        );
+    });
+    it("have a help icon", () => {
+        cy.get('$[data-cy="help-icon"]');
+    });
+    it("have a profile icon", () => {
+        cy.get('$[data-cy="profile-icon"]');
+    });
+});
