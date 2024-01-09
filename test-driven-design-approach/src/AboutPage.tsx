b86b371ec1cb1732da7aaa9d74daa43862043872
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-01-09 09:53:22 +0100

    move pages to own folder

--- test-driven-design-approach/src/AboutPage.tsx
+++
@@ -1,7 +0,0 @@
-const AboutPage = () => {
-    const title = "About";
-
-    return <h2>{title}</h2>;
-};
-
-export default AboutPage;
--- test-driven-design-approach/src/ApplicationPage.tsx
+++
@@ -1,7 +0,0 @@
-const ApplicationPage = () => {
-    const title = "Application";
-
-    return <h2>{title}</h2>;
-};
-
-export default ApplicationPage;
