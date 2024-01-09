1117b5082bd8bbd7e94e7d84e7e9b434c33c2aec
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-01-09 09:54:23 +0100

    Add router

--- test-driven-design-approach/src/index.tsx
+++ test-driven-design-approach/src/index.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Header from "./component/Header";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import { Divider } from "@react-md/divider";
+import Header from "./component/Header";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
+
 root.render(
-    <React.StrictMode>
+    <BrowserRouter>
         <Header />
-        <Divider />
-        <App />
-    </React.StrictMode>
+    </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
