17123e905faaccd3d528d5952ff91534368d25d0
Ina F. Pedersen <42718910+InaFPedersen@users.noreply.github.com>
2024-01-09 09:54:42 +0100

    add content to about page

---
+++ test-driven-design-approach/src/pages/AboutPage.tsx
@@ -0,0 +1,51 @@
+import { Button, Form } from "react-bootstrap";
+import SideSheet from "../component/SideSheet";
+import { useState } from "react";
+
+const About = () => {
+    const title = "About";
+    const [openSideSheet, setSideSheetOpen] = useState(false);
+
+    const ReadMore = () => {
+        setSideSheetOpen(!openSideSheet);
+    };
+
+    return (
+        <div>
+            <h2>{title}</h2>
+
+            <p>Lorum ipsum</p>
+
+            <Button onClick={ReadMore} data-cy="read-more">
+                Read more
+            </Button>
+
+            {openSideSheet ? (
+                <div>
+                    <SideSheet data-cy="side-sheet" />{" "}
+                    <Button
+                        data-cy="close-button"
+                        style={{
+                            backgroundColor: "lightblue",
+                            width: "100px",
+                            height: "50px",
+                            fontWeight: "bold",
+                            fontSize: "18px",
+                            float: "right",
+                            zIndex: "1001",
+                            marginRight: "-760px",
+                            marginTop: "1100px",
+                        }}
+                        onClick={ReadMore}
+                    >
+                        Close
+                    </Button>
+                </div>
+            ) : (
+                <></>
+            )}
+        </div>
+    );
+};
+
+export default About;
